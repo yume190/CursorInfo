01dac5302dffc9558db2cece28cd32ee0b1c09dc
yume190@gmail.com <yume>
2023-11-06 17:12:45 +0800

    Initial Commit

---
+++ CursorInfo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CursorInfo
+//
+//  Created by Tangram Yume on 2023/11/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CursorInfo/CursorInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorInfoApp.swift
+//  CursorInfo
+//
+//  Created by Tangram Yume on 2023/11/6.
+//
+
+import SwiftUI
+
+@main
+struct CursorInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
